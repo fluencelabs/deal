5d64319117907eb9a98236258461d4f2d9453934
elshan.eth <elshan.eth>
2022-10-09 21:14:12 +0400

    change arch

--- aurora/contracts/DealFactory.sol
+++ aurora/contracts/DealFactory.sol
@@ -1,9 +1,30 @@
 pragma solidity ^0.8.17;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./AquaProxy.sol";
+import "./Deal.sol";
+
 contract DealFactory {
-    //TODO: add logic
-    function createDeal(address owner, address paymentToken)
-        external
-        returns (address)
-    {}
+    //TODO: add editable params
+    address public immutable daoAddress;
+    AquaProxy public immutable aquaProxy;
+    IERC20 public immutable fluenceToken;
+
+    constructor(
+        address daoAddress_,
+        AquaProxy aquaProxy_,
+        IERC20 fluenceToken_
+    ) {
+        daoAddress = daoAddress_;
+        aquaProxy = aquaProxy_;
+        fluenceToken = fluenceToken_;
+    }
+
+    function createDeal(IERC20 paymentToken_) external returns (address) {
+        //TODO: add event
+        return
+            address(
+                new Deal(paymentToken_, daoAddress, aquaProxy, fluenceToken)
+            );
+    }
 }
--- aurora/contracts/Staking.sol
+++ aurora/contracts/Staking.sol
@@ -1,62 +1,78 @@
 pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./AquaProxy.sol";
 
 contract Staking {
-    uint constant SLASH_FACTOR = 100;
-
-    mapping(address => uint) public stakingBalances;
-    mapping(address => uint) public lastSlashIds;
-
-    address public daoAddress;
-    IERC20 private _fluenceToken;
-
-    //TODO
-    /*
-    constructor(IERC20 fluenceToken_) {
-        _fluenceToken = fluenceToken_;
-    }*/
-
-    function stake(uint amount) external {
-        stakingBalances[msg.sender] += amount;
-        _fluenceToken.transferFrom(msg.sender, address(this), amount);
-
-        _callToNear("add_validator");
+    struct Validator {
+        uint balance;
+        bool isActive;
     }
 
-    function exit(address account) external {
-        require(!isSlashed(account), "Account already slashed");
-        if (isSlashed(account)) {
-            _slash(account);
-        } else {
+    uint public constant MIN_STAKE = 1000;
+    uint constant SLASH_FACTOR = 100;
+
+    mapping(address => Validator) public validators;
+
+    address public immutable daoAddress;
+    AquaProxy public immutable aquaProxy;
+    IERC20 public immutable fluenceToken;
+
+    constructor(
+        address daoAddress_,
+        AquaProxy aquaProxy_,
+        IERC20 fluenceToken_
+    ) {
+        daoAddress = daoAddress_;
+        aquaProxy = aquaProxy_;
+        fluenceToken = fluenceToken_;
+    }
+
+    function stake(uint amount) external {
+        require(amount >= MIN_STAKE, "Amount is too small");
+
+        Validator memory validator = validators[msg.sender];
+
+        validator.balance += amount;
+        if (!validator.isActive) {
+            validator.isActive = true;
+        }
+
+        validators[msg.sender] = validator;
+
+        fluenceToken.transferFrom(msg.sender, address(this), amount);
+    }
+
+    function exit(AquaProxy.Particle calldata particle, address account)
+        external
+    {
+        bool isValid = aquaProxy.isValidScript(particle);
+
+        if (isValid) {
             _exit(account);
+        } else {
+            _slash(account);
         }
     }
 
     function _slash(address account) private {
-        uint slashAmount = (stakingBalances[account] / 100) * SLASH_FACTOR;
+        Validator memory validator = validators[account];
 
-        stakingBalances[account] -= slashAmount;
-        _fluenceToken.transferFrom(address(this), daoAddress, slashAmount);
+        uint slashAmount = (validator.balance / 100) * SLASH_FACTOR;
 
-        //TODO: callback to near
-        _callToNear("unslash");
+        validator.balance -= slashAmount;
+
+        validators[account] = validator;
+        fluenceToken.transferFrom(address(this), daoAddress, slashAmount);
     }
 
     function _exit(address account) private {
-        uint256 balance = stakingBalances[account];
-        stakingBalances[account] = 0;
+        Validator memory validator = validators[account];
 
-        _fluenceToken.transferFrom(address(this), account, balance);
+        uint balance = validator.balance;
+        validator.balance = 0;
 
-        _callToNear("remove_validator");
+        validators[account] = validator;
+        fluenceToken.transferFrom(address(this), daoAddress, balance);
     }
-
-    function isSlashed(address account) private view returns (bool) {
-        //TODO: call to near
-        return false;
-    }
-
-    //TODO: callback to near
-    function _callToNear(string memory method) internal {}
 }
