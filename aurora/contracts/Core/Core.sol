385e774f47685f1e1405904daa277f7e13383b1a
elshan.eth <raiman.elshan@gmail.com>
2022-12-02 19:10:53 +0400

    update

---
+++ aurora/contracts/Core/Core.sol
@@ -0,0 +1,64 @@
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
+import "../AuroraSDK/AuroraSdk.sol";
+import "./AquaProxy.sol";
+import "./EpochManager.sol";
+
+contract CoreState {
+    IERC20 public fluenceToken;
+    AquaProxy public aquaProxy;
+    uint public withdrawTimeout;
+    uint public slashFactor;
+    uint public updateSettingsTimeout;
+    EpochManager public epochManager;
+}
+
+contract Core is OwnableUpgradeable, CoreState, UUPSUpgradeable {
+    function initialize(
+        IERC20 fluenceToken_,
+        AquaProxy aquaProxy_,
+        uint withdrawTimeout_,
+        uint slashFactor_,
+        uint updateSettingsTimeout_,
+        EpochManager epochManager_
+    ) public initializer {
+        fluenceToken = fluenceToken_;
+        aquaProxy = aquaProxy_;
+        withdrawTimeout = withdrawTimeout_;
+        slashFactor = slashFactor_;
+        updateSettingsTimeout = updateSettingsTimeout_;
+        epochManager = epochManager_;
+
+        __Ownable_init();
+    }
+
+    function setFluenceToken(IERC20 fluenceToken_) external onlyOwner {
+        fluenceToken = fluenceToken_;
+    }
+
+    function setAquaProxy(AquaProxy aquaProxy_) external onlyOwner {
+        aquaProxy = aquaProxy_;
+    }
+
+    function setWithdrawTimeout(uint withdrawTimeout_) external onlyOwner {
+        withdrawTimeout = withdrawTimeout_;
+    }
+
+    function setSlashFactor(uint slashFactor_) external onlyOwner {
+        slashFactor = slashFactor_;
+    }
+
+    function setUpdateSettingsTimeout(
+        uint updateSettingsTimeout_
+    ) external onlyOwner {
+        updateSettingsTimeout = updateSettingsTimeout_;
+    }
+
+    function setEpochManager(EpochManager epochManager_) external onlyOwner {
+        epochManager = epochManager_;
+    }
+
+    function _authorizeUpgrade(address newImplementation) internal override {}
+}
