b8a93c9b0d62e093c564a858fd8b1bd479feba8f
elshan.eth <raiman.elshan@gmail.com>
2023-01-05 16:17:44 +0400

    add dockerfile, fix package.json and add readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# Deal monorepo
+
+## Paths
+
+- aurora - deal contracts system on Aurora chain
+- cli - simple cli for using deal
+- near - part of deal contracts system on Near chain
--- cli/src/config/config.ts
+++ cli/src/config/config.ts
@@ -1,6 +1,6 @@
 export const CONFIG = {
   ethereumNodeUrl: "http://127.0.0.1:8545/",
-  coreAddress: "0x3Af511B1bdD6A0377e23796aD6B7391d8De68636",
-  dealFactoryAddress: "0xBD2fe040D03EB1d1E5A151fbcc19A03333223019",
-  developerFaucetAddress: "0x834Ea01e45F9b5365314358159d92d134d89feEb",
+  coreAddress: "0x10537D7bD661C9c34F547b38EC662D6FD482Ae95",
+  dealFactoryAddress: "0xfb6dAB6200b8958C2655C3747708F82243d3F32E",
+  developerFaucetAddress: "0x8D75F9F7f4F4C4eFAB9402261bC864f21DF0c649",
 };
