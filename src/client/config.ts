c5870d9a280b4727cb73808858f232044bb18dd6
elshan_eth <raiman.elshan@gmail.com>
2023-08-03 21:54:47 +0200

    fix load deployment in client

--- src/client/config.ts
+++ src/client/config.ts
@@ -1,33 +1,37 @@
-import { address as localGlobalConfigAddress } from "../deployments/localhost/GlobalConfig.json";
-import { address as localDealFactoryAddress } from "../deployments/localhost/Factory.json";
-import { address as localFLTTokenAddress } from "../deployments/localhost/FLT.json";
-
-import { address as globalConfigAddress } from "../deployments/testnet/GlobalConfig.json";
-import { address as dealFactoryAddress } from "../deployments/testnet/Factory.json";
-import { address as fltTokenAddress } from "../deployments/testnet/FLT.json";
-
 export const CONTRACTS_ENV = ["testnet", "local"] as const;
 
 export type ContractsENV = (typeof CONTRACTS_ENV)[number];
 
-type ChainConfig = {
+export type ChainConfig = {
     globalConfig: string;
     dealFactoryAddress: string;
     fltToken: string;
     chainId: number;
 };
 
-export const DEAL_CONFIG: Record<ContractsENV, ChainConfig> = {
-    testnet: {
-        globalConfig: globalConfigAddress,
-        dealFactoryAddress: dealFactoryAddress,
-        fltToken: fltTokenAddress,
-        chainId: 1313161555,
+export const DEAL_CONFIG: Record<ContractsENV, () => Promise<ChainConfig>> = {
+    testnet: async (): Promise<ChainConfig> => {
+        const globalConfig = await import("../deployments/testnet/GlobalConfig.json");
+        const factory = await import("../deployments/testnet/Factory.json");
+        const flt = await import("../deployments/testnet/FLT.json");
+
+        return {
+            globalConfig: globalConfig.address,
+            dealFactoryAddress: factory.address,
+            fltToken: flt.address,
+            chainId: 1313161555,
+        };
     },
-    local: {
-        globalConfig: localGlobalConfigAddress,
-        dealFactoryAddress: localDealFactoryAddress,
-        fltToken: localFLTTokenAddress,
-        chainId: 31337,
+    local: async (): Promise<ChainConfig> => {
+        const globalConfig = await import("../deployments/localhost/GlobalConfig.json");
+        const factory = await import("../deployments/localhost/Factory.json");
+        const flt = await import("../deployments/localhost/FLT.json");
+
+        return {
+            globalConfig: globalConfig.address,
+            dealFactoryAddress: factory.address,
+            fltToken: flt.address,
+            chainId: 31337,
+        };
     },
 };
