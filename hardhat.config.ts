4925b0cb4dfbf715703f17ff31c23cc257ca7768
Anatoly Laskaris <github_me@nahsi.dev>
2023-08-08 18:42:38 +0300

    Set interval

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -20,7 +20,8 @@ const config: HardhatUserConfig = {
         hardhat: {
             mining: {
               auto: true,
-            }
+              interval: 1000,
+            },
             accounts: {
                 passphrase: "test test test test claim trade stairs crew inspire obey veteran budget",
             },
