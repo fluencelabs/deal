ee4ad0febdfecbd5e4fc2b7197e2ef2ae3563105
elshan_eth <raiman.elshan@gmail.com>
2023-09-07 18:43:01 +0200

    feat: whitelist & blacklist

--- contracts/deal/interfaces/IConfigModule.sol
+++ contracts/deal/interfaces/IConfigModule.sol
@@ -7,6 +7,14 @@ import "../base/Types.sol";
 import "../../global/interfaces/IGlobalConfig.sol";
 
 interface IConfigModule {
+    enum AccessType {
+        NONE,
+        WHITELIST,
+        BLACKLIST
+    }
+
+    event AppCIDChanged(CIDV1 newAppCID);
+
     function initialize(
         IERC20 paymentToken_,
         uint256 pricePerEpoch_,
@@ -14,28 +22,42 @@ interface IConfigModule {
         CIDV1 calldata appCID_,
         uint256 minWorkers_,
         uint256 targetWorkers_,
-        CIDV1[] calldata effectorWasmsCids_
+        CIDV1[] calldata effectors_,
+        AccessType accessType_,
+        address[] calldata accessList_
     ) external;
 
+    // ------------------ VIEWS ---------------------
     function globalConfig() external view returns (IGlobalConfig);
-
-    function fluenceToken() external view returns (IERC20);
 
     function paymentToken() external view returns (IERC20);
 
-    function pricePerEpoch() external view returns (uint256);
-
-    function requiredCollateral() external view returns (uint256);
-
-    function appCID() external view returns (CIDV1 memory);
-
-    function minWorkers() external view returns (uint256);
-
-    function targetWorkers() external view returns (uint256);
+    function fluenceToken() external view returns (IERC20);
 
     function creationBlock() external view returns (uint256);
 
+    function pricePerEpoch() external view returns (uint256);
+
+    function targetWorkers() external view returns (uint256);
+
+    function requiredCollateral() external view returns (uint256);
+
+    function minWorkers() external view returns (uint256);
+
     function effectors() external view returns (CIDV1[] memory);
 
+    function accessType() external view returns (AccessType);
+
+    function isInAccessList(address addr) external view returns (bool);
+
+    function getAccessList() external view returns (address[] memory);
+
+    function appCID() external view returns (CIDV1 memory);
+
+    // ------------------ MUTABLES ------------------
     function setAppCID(CIDV1 calldata appCID_) external;
+
+    function changeAccessType(AccessType accessType_) external;
+
+    function removeFromAccessList(address addr) external;
 }
--- contracts/global/interfaces/IFactory.sol
+++ contracts/global/interfaces/IFactory.sol
@@ -3,6 +3,7 @@
 pragma solidity ^0.8.19;
 
 import "../../deal/base/Types.sol";
+import "../../deal/interfaces/IConfigModule.sol";
 
 interface IFactory {
     function isDeal(address addr) external view returns (bool);
@@ -11,6 +12,8 @@ interface IFactory {
         uint256 minWorkers_,
         uint256 targetWorkers_,
         CIDV1 calldata appCID_,
-        CIDV1[] calldata effectors
+        CIDV1[] calldata effectors,
+        IConfigModule.AccessType accessType_,
+        address[] calldata accessList_
     ) external returns (address);
 }
