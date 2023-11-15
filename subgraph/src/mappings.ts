fd78669cffdbd86ab5de8cc136b298646c4cd5bb
alcibiadescleinias <ivan.cheprasov@phystech.edu>
2023-11-15 17:54:49 +0100

    resolve Offer 1.2 screen

--- subgraph/src/mappings.ts
+++ subgraph/src/mappings.ts
@@ -1,28 +1,46 @@
 // File should be writen after `npm run compile` is run or you will encounter syntax and import errors.
-import { MarketOffer } from "../generated/schema";
-import {MarketOfferRegistered, CoreImpl, ComputeUnitCreated} from '../generated/Core/CoreImpl'
+// Note: handlers named as in the contract methods
+// TODO: reorganise to sub models of mapping module.
+
+import {
+    MarketOfferRegistered,
+    CoreImpl,
+    ComputeUnitCreated,
+    MarketOfferRegisteredEffectorsStruct, PeerCreated
+} from '../generated/Core/CoreImpl'
 // import {extractIdFromEvent} from "./utils";
-import {getTokenSymbol} from "./tokenNames";
-import {getOrCreateMarketOffer} from "./models";
+import {getTokenSymbol} from "./networkConstants";
+import {getOrCreateEffector, getOrCreateOffer, getOrCreatePeer} from "./models";
+import {BigInt, Bytes, ByteArray} from "@graphprotocol/graph-ts";
 
 import { log } from '@graphprotocol/graph-ts'
 // log.info('My value is: {}', [myValue])
 
 
-// TODO: where/what is maxCollateralPerWorker?
-export function handleRegisterMarketOffer(event: MarketOfferRegistered): void {
+export function handleMarketOfferRegistered(event: MarketOfferRegistered): void {
     // Events: MarketOfferRegistered
     // Children events:
     // - emit PeerCreated(offerId, peer.peerId);
     // - emit ComputeUnitCreated(offerId, peerId, unitId);
-    // Screen: List of offers
 
-    let entity = getOrCreateMarketOffer(event.params.offerId.toHex())
+    let entity = getOrCreateOffer(event.params.offerId.toHex())
+
+    let effectorEntities: Array<string> = []
+    const fetchedEffectors = event.params.effectors
+    for (let i=0; i < fetchedEffectors.length; i++) {
+
+        let tup: MarketOfferRegisteredEffectorsStruct
+        tup = fetchedEffectors[i]
+        const cid = tup.toString() + tup.toString()
+        const effector = getOrCreateEffector(cid)
+        effectorEntities.push(effector.id)
+    }
 
     entity.createdAt = event.block.timestamp
     entity.provider = event.params.owner
     entity.tokenSymbol = getTokenSymbol(event.params.paymentToken)
     entity.pricePerEpoch = event.params.minPricePerWorkerEpoch
+    entity.effectors = effectorEntities
 
     // TODO: how to Handle ComputeUnitCreated events as well via transaction logs instead of contract call.
     //  mb the flow via separate handler is more natural.
@@ -34,28 +52,27 @@ export function handleRegisterMarketOffer(event: MarketOfferRegistered): void {
     entity.save();
 }
 
+// It updates Peer and Offer.
 export function handleComputeUnitCreated(event: ComputeUnitCreated): void {
-    let entity = getOrCreateMarketOffer(event.params.offerId.toHex())
+    // Parent events:
+    // - emit PeerCreated(offerId, peer.peerId);
+    // - emit MarketOfferRegistered
+    let offer = getOrCreateOffer(event.params.offerId.toHex())
     // or maybe save everything into context and then count:
     //  https://thegraph.com/docs/en/developing/creating-a-subgraph/#data-source-context.
-    entity.computeUnits += 1
+    offer.computeUnitsSum += 1
+
+    let peer = getOrCreatePeer(event.params.peerId.toHex())
+    peer.computeUnits += 1
+
+    peer.save()
+    offer.save()
 }
 
-// TODO: handle other update queries: addFreeUnits
-// Parse current contract data:
-//     let contract = CoreImpl.bind(event.address)
-//     let erc20Symbol = contract.symbol()
+// It updates Peer and Offer.
+export function handlePeerCreated(event: PeerCreated): void {
+    let entity = getOrCreatePeer(event.params.peerId.toHex())
 
-// export function handleComputeUnitCreated(event: ComputeUnitCreated): void {
-//     let entity = new MarketOffer(extractIdFromEvent(event));
-//
-// }
-
-// type MarketOffer @entity {
-//   id: ID!
-//   createdAt: BigInt!
-//   provider: String!
-//   computeUnits: Int
-//   pricePerEpoch: Int
-//   maxCollateralPerWorker: Int
-// }
+    entity.offer = event.params.offerId.toHex()
+    entity.save()
+}
--- subgraph/src/models.ts
+++ subgraph/src/models.ts
@@ -1,19 +1,47 @@
-import {MarketOffer} from "../generated/schema";
+import {Effector, Offer, Peer} from "../generated/schema";
 import {BigInt, Bytes, ByteArray} from "@graphprotocol/graph-ts";
 
 // TODO: make generic.
-export function getOrCreateMarketOffer(
+export function getOrCreateOffer(
     offerId: string,
-    ): MarketOffer {
-    let entity = MarketOffer.load(offerId)
+    ): Offer {
+    let entity = Offer.load(offerId)
 
     if (entity == null) {
-        entity = new MarketOffer(offerId)
+        entity = new Offer(offerId)
         entity.createdAt = BigInt.fromI32(0)
         entity.provider = Bytes.fromHexString("0")
         entity.tokenSymbol = ""
         entity.pricePerEpoch = BigInt.fromI32(0)
+        // entity.effectors = []
         entity.save()
     }
-    return entity as MarketOffer
+    return entity as Offer
+}
+
+// TODO: add description mapper.
+const DEFAULT_EFFECTOR_DESCRIPTION = "DEFAULT_EFFECTOR_DESCRIPTION"
+
+export function getOrCreateEffector(cid: string): Effector {
+    let entity = Effector.load(cid)
+
+    if (entity == null) {
+        entity = new Effector(cid)
+        entity.CID = Bytes.fromHexString("0")
+        entity.Description = DEFAULT_EFFECTOR_DESCRIPTION
+        entity.save()
+    }
+    return entity as Effector
+}
+
+export function getOrCreatePeer(peerId: string): Peer {
+    let entity = Peer.load(peerId)
+
+    if (entity == null) {
+        entity = new Peer(peerId)
+        entity.computeUnits = 0
+        entity.offer = ""
+        entity.save()
+    }
+    return entity as Peer
 }
