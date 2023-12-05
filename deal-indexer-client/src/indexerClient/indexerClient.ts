e580cde0fc63c725e24b3ddb9fd9c29aafe0c3d8
alcibiadescleinias <ivan.cheprasov@phystech.edu>
2023-12-05 18:41:52 +0100

    log error properly

--- deal-indexer-client/src/indexerClient/indexerClient.ts
+++ deal-indexer-client/src/indexerClient/indexerClient.ts
@@ -4,6 +4,6 @@ import { execute } from "../../.graphclient";
 // TODO: add responseType
 export async function requestIndexer(documentNode: DocumentNode, options: unknown): Promise<unknown | null> {
     const result = await execute(documentNode, options);
-    console.info("[requestIndexer] Sent %s, Got: %s", documentNode, result);
+    console.info("[requestIndexer] Sent %s, Got: %s", documentNode, JSON.stringify(result));
     return result.data;
 }
