fd825aa71858eb0b1c7e633eecfdc4b28c40844a
alcibiadescleinias <ivan.cheprasov@phystech.edu>
2023-12-14 16:39:34 +0100

    add notes into abc class

--- ts-client/src/dealExplorerClient/rpcClients/multicall3ContractClient.ts
+++ ts-client/src/dealExplorerClient/rpcClients/multicall3ContractClient.ts
@@ -3,7 +3,7 @@ import { IMulticall3__factory, type IMulticall3 } from "../../index.js";
 
 export type Multicall3ContractCall = { target: string, allowFailure: boolean, callData: string };
 export type Aggregate3Response = { success: boolean; returnData: string };
-export type TxResultsConverter<T> = (result: Result, ...opt: any[]) => T;
+export type TxResultsConverter<T> = (result: Result | null, ...opt: any[]) => T;
 
 /*
  * @description Client to be inherited from to work with Multicall3 contract to perform batch calls.
@@ -17,6 +17,10 @@ export abstract class Multicall3ContractClient {
         this._multicall3Contract = IMulticall3__factory.connect(multicall3ContractAddress, this._caller);
     }
 
+    /*
+     * @dev Note, If one of the transcation is unsuccessfull it sends null to txResultsConverter.
+     * @dev  Thus, null should be catched approptitery in txResultsConverter.
+    */
     async _callBatch<ConvertTo>(
         callsEncoded: Multicall3ContractCall[],
         callResultsInterface: Interface,
@@ -29,20 +33,29 @@ export abstract class Multicall3ContractClient {
         let decodedResults: Array<ConvertTo> = []
         console.info("Decode and convert all results with converter %s", txResultsConverter.name)
         for (const rawResult of multicallContractCallResults) {
-            console.info('Success status: %s', rawResult.success)
-            console.debug('Raw data: %s', rawResult.returnData)
+            const status = rawResult.success;
+            const rawReturnData = rawResult.returnData;
+            console.info('Success status: %s', status)
+            console.debug('Raw data: %s', rawReturnData)
 
-            const decoded = callResultsInterface.decodeFunctionResult(contractMethod, rawResult.returnData)
-            console.debug('Got after decoding: %s', decoded)
-            decodedResults.push(
-                txResultsConverter(decoded)
-            )
+            if (!status) {
+                decodedResults.push(
+                    txResultsConverter(null)
+                )
+            } else {
+                const decoded = callResultsInterface.decodeFunctionResult(contractMethod, rawReturnData)
+                console.debug('Got after decoding: %s', decoded)
+                decodedResults.push(
+                    txResultsConverter(decoded)
+                )
+            }
         }
         console.groupEnd();
         return decodedResults
     }
 
-    _decodeToString(result: Result, defaultNoResult: string = ""): string {
+    // Basic implementation of TxResultsConverter.
+    _decodeToString(result: Result | null, defaultNoResult: string = ""): string {
         if (!result) {
             return defaultNoResult;
         }
