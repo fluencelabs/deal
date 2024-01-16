3c31ca1dc2a5b59b2352199560c70048b0b2b3ee
elshan_eth <raiman.elshan@gmail.com>
2024-01-16 18:55:15 +0100

    fix bug

--- ts-client/tests/unittests/client.test.ts
+++ ts-client/tests/unittests/client.test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from "vitest";
-import {getDeployment} from "../../src";
+import { getDeployment } from "../../src";
 import { ethers } from "ethers";
 //  TODO: it is better to have integration tests for DealClient for calls: getMarket/etc.
 
