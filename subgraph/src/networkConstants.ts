20a1c92ea0ca8125ae6a61f5d49da8ad15b7e07b
alcibiadescleinias <ivan.cheprasov@phystech.edu>
2024-01-16 19:26:30 +0100

    provider name convention: Provider 0x22cda5b

--- subgraph/src/networkConstants.ts
+++ subgraph/src/networkConstants.ts
@@ -1,6 +1,6 @@
 // TODO: add proper provider name resolver.
 export function getProviderName(providerAddress: string): string {
-  let name = "Awesome Provider";
+  let name = "Provider" + providerAddress.slice(0, 8);
   if (providerAddress === "0x0000000000000000000000000000000000000000") {
     name = "ZERO Provider";
   }
