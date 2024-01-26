32a9d8a6cd2cb06e03c34934f1b8316bc83771e3
elshan_eth <raiman.elshan@gmail.com>
2024-01-26 13:56:44 +0100

    fix: Market interfaces

--- src/core/modules/market/interfaces/IMarket.sol
+++ src/core/modules/market/interfaces/IMarket.sol
@@ -4,9 +4,10 @@ pragma solidity ^0.8.19;
 
 import "./IDealFactory.sol";
 import "./IOffer.sol";
+import "./IMatcher.sol";
 
 /// @title Market contract interface
 /// @dev Market contract is responsible for managing the deals and offers
-interface IMarket is IDealFactory, IOffer {
+interface IMarket is IDealFactory, IOffer, IMatcher {
     function initialize(IDeal dealImpl) external;
 }
