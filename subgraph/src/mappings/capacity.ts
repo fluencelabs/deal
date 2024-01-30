b8a6a7dd619df55df43e114913cd88f32b590b86
alcibiadescleinias <ivan.cheprasov@phystech.edu>
2024-01-30 11:49:00 +0100

    update subgraphL store CU links to CC

--- subgraph/src/mappings/capacity.ts
+++ subgraph/src/mappings/capacity.ts
@@ -1,9 +1,14 @@
 import {
-  CapacityCommitmentStatus,
-  createOrLoadGraphNetwork,
+  CapacityCommitmentStatus, createOrLoadCapacityCommitmentToComputeUnit,
+  createOrLoadGraphNetwork, UNO_BIG_INT,
   ZERO_BIG_INT
 } from "../models";
-import {CapacityCommitment, Peer, Provider} from "../../generated/schema";
+import {
+  CapacityCommitment,
+  ComputeUnit,
+  Peer,
+  Provider
+} from "../../generated/schema";
 import {
   CollateralDeposited,
   CommitmentActivated,
@@ -56,15 +61,19 @@ export function handleCommitmentCreated(event: CommitmentCreated): void {
   commitment.failedEpoch = ZERO_BIG_INT
   commitment.exitedUnitCount = 0
   commitment.nextCCFailedEpoch = ZERO_BIG_INT
-  commitment.unitCount = 0
+  commitment.computeUnitsCount = 0
   commitment.nextAdditionalActiveUnitCount = 0
   commitment.snapshotEpoch = ZERO_BIG_INT
   commitment.deleted = false;
   commitment.save()
 
   let peer = Peer.load(event.params.peerId.toHex()) as Peer;
-  peer.currentCapacityCommitment = commitment.id
+  peer.currentCapacityCommitment = commitment.id;
   peer.save()
+
+  let graphNetwork = createOrLoadGraphNetwork();
+  graphNetwork.capacityCommitmentsTotal = graphNetwork.capacityCommitmentsTotal.plus(UNO_BIG_INT);
+  graphNetwork.save();
 }
 
 // It is supposed that collateral deposited at the same time as commitment activated
@@ -75,7 +84,15 @@ export function handleCommitmentActivated(event: CommitmentActivated): void {
   commitment.startEpoch = event.params.startEpoch
   commitment.endEpoch = event.params.endEpoch
   commitment.activeUnitCount = event.params.unitIds.length
-  commitment.unitCount = event.params.unitIds.length
+
+  commitment.computeUnitsCount = event.params.unitIds.length
+  for (let i=0; i < event.params.unitIds.length; i++) {
+    const computeUnitId = event.params.unitIds[i].toHex()
+    // We rely on contract logic that it is not possible to emit event with not existing CUs.
+    //  Also, we relay that previously we save computeUnitId successfully in prev. handler.
+    const computeUnit = ComputeUnit.load(computeUnitId) as ComputeUnit
+    createOrLoadCapacityCommitmentToComputeUnit(commitment.id, computeUnit.id)
+  }
   commitment.nextAdditionalActiveUnitCount = 0
   const graphNetwork = createOrLoadGraphNetwork();
   commitment.snapshotEpoch = calculateEpoch(
@@ -86,7 +103,7 @@ export function handleCommitmentActivated(event: CommitmentActivated): void {
 
   const _calculatedFailedEpoch = calculateNextFailedCCEpoch(
     BigInt.fromString(graphNetwork.capacityMaxFailedRatio.toString()),
-    BigInt.fromString(commitment.unitCount.toString()),
+    BigInt.fromString(commitment.computeUnitsCount.toString()),
     BigInt.fromString(commitment.activeUnitCount.toString()),
     BigInt.fromString(commitment.nextAdditionalActiveUnitCount.toString()),
     BigInt.fromString(commitment.totalCUFailCount.toString()),
@@ -141,7 +158,7 @@ export function handleCommitmentStatsUpdated(event: CommitmentStatsUpdated): voi
   // Calculate next failed epoch.
   const _calculatedFailedEpoch = calculateNextFailedCCEpoch(
     BigInt.fromString(graphNetwork.capacityMaxFailedRatio.toString()),
-    BigInt.fromString(commitment.unitCount.toString()),
+    BigInt.fromString(commitment.computeUnitsCount.toString()),
     BigInt.fromString(commitment.activeUnitCount.toString()),
     BigInt.fromString(commitment.nextAdditionalActiveUnitCount.toString()),
     BigInt.fromString(commitment.totalCUFailCount.toString()),
