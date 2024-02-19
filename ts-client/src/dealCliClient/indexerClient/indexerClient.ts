9fdc6f89d67594f04542c5dfabfaa483976b9b17
Ivan <39461389+AlcibiadesCleinias@users.noreply.github.com>
2024-02-19 13:29:25 +0100

    feat: indexer for cli - get offer with some data [chain-356] (#263)
    * resolve CHAIN-358
    
    * forgot to add client itself
    
    * add fix to basic class
    
    * add contribute notes

---
+++ ts-client/src/dealCliClient/indexerClient/indexerClient.ts
@@ -0,0 +1,19 @@
+import type {
+  OfferQueryQueryVariables,
+  Sdk as OffersSdk,
+} from "./queries/offers-query.generated.js";
+import { getSdk as getOffersSdk } from "./queries/offers-query.generated.js";
+import { IndexerClientABC } from "../../indexerClient/indexerClientABC.js";
+import type { ContractsENV } from "../../client/config.js";
+
+export class IndexerClient extends IndexerClientABC {
+  private offersClient: OffersSdk;
+  constructor(network: ContractsENV, indexerUrl?: string) {
+    super(network, indexerUrl);
+    this.offersClient = getOffersSdk(this._graphqlClient);
+  }
+
+  async getOffer(variables: OfferQueryQueryVariables) {
+    return await this.offersClient.OfferQuery(variables);
+  }
+}
