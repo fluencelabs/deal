216787a163f5e910a12309dc12bb6d48528900cd
alcibiadescleinias <ivan.cheprasov@phystech.edu>
2024-02-21 20:34:38 +0100

    fix package: generated files

--- ts-client/src/dealExplorerClient/indexerClient/queries/peers-query.generated.ts
+++ ts-client/src/dealExplorerClient/indexerClient/queries/peers-query.generated.ts
@@ -4,6 +4,7 @@ import * as Types from '../generated.types.js';
 
 import { GraphQLClient } from 'graphql-request';
 import type { RequestOptions } from 'graphql-request';
+import gql from 'graphql-tag';
 type GraphQLClientRequestHeaders = RequestOptions['requestHeaders'];
 export type PeerQueryQueryVariables = Types.Exact<{
   id: Types.Scalars['ID']['input'];
