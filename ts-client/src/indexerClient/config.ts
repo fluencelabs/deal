c425af94585f8d2ba21f4ad39c73ff3c2da3e799
alcibiadescleinias <ivan.cheprasov@phystech.edu>
2024-02-29 19:27:00 +0100

    update stage

--- ts-client/src/indexerClient/config.ts
+++ ts-client/src/indexerClient/config.ts
@@ -17,7 +17,7 @@ export const getIndexerUrl = (env: ContractsENV) => {
       break;
     case "stage":
       indexerUrl =
-        "https://graph-node-stage.fluence.dev/subgraphs/name/fluence-deal-contracts-c7009d34";
+        "https://graph-node-stage.fluence.dev/subgraphs/name/fluence-deal-contracts-1bba6637";
       break;
     case "local":
       indexerUrl =
