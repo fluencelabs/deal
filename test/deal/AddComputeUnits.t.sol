d0c9f266b55d3d3a286358cff0428d5b751e35a0
Elshan Dzhafarov <raiman.elshan@gmail.com>
2024-03-05 13:16:53 +0100

    Update test/deal/AddComputeUnits.t.sol
    Co-authored-by: Aleksey Proshutinskiy <justprosh@users.noreply.github.com>

--- test/deal/AddComputeUnits.t.sol
+++ test/deal/AddComputeUnits.t.sol
@@ -68,7 +68,7 @@ contract AddComputeUnits is Test {
         uint256 targetWorkers = 10;
         Deal deal = deployment.deployDealWithoutFactory(2, targetWorkers, 1, 1 ether, 10 ether);
 
-        assertEq(uint256(deal.getStatus()), uint256(IDeal.Status.NOT_ENOUGH_WORKERS), "status should be INACTIVE");
+        assertEq(uint256(deal.getStatus()), uint256(IDeal.Status.NOT_ENOUGH_WORKERS), "status should be NOT_ENOUGH_WORKERS");
         assertEq(deal.getMaxPaidEpoch(), 0, "maxPaidEpoch should be 0");
 
         (address[] memory computeProviders, bytes32[] memory peerIds, bytes32[] memory unitIds) =
