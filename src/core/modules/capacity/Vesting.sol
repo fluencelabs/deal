41c0d20aa680f7857e2ebcb3e83c8492825e9cfd
Elshan Dzhafarov <raiman.elshan@gmail.com>
2024-03-15 20:19:11 +0100

    Fix: vesting (#354)
    * fix Vesting.add
    
    * add comment to assert
    
    * fmt fix
    
    * improve assert
    
    ---------
    
    Co-authored-by: Valery Antopol <valery.antopol@gmail.com>
    Co-authored-by: Mike Voronov <michail.vms@gmail.com>

--- src/core/modules/capacity/Vesting.sol
+++ src/core/modules/capacity/Vesting.sol
@@ -52,25 +52,48 @@ library Vesting {
     ) internal {
         uint256 length = self.items.length;
         uint256 rewardPerPeriod = reward / vestingPeriodCount;
+
+        // find next epoch divisible by vestingPeriodDuration
         uint256 startVestingEpoch = (startEpoch + (vestingPeriodDuration - startEpoch % vestingPeriodDuration));
-
         int256 index = _findClosest(self, startVestingEpoch);
-        uint256 cumulativeAmount = 0;
 
+        // if startVestingEpoch is lower than all epochs in self.items, then it will be added to the back of the array,
+        // making self.items[i] non-monotonous
+        if (index == -1 && length != 0) {
+            revert("startEpoch is below than all seen epochs");
+        }
+
+        // if startVestingEpoch is larger than any epoch in items, add a corresponding item without increasing
+        // cumulativeAmount, without it first part of the reward will be added to an epoch in the past
+        if (index >= 0 && self.items[uint256(index)].epoch < startVestingEpoch) {
+            uint256 prevCumulativeAmount = self.items[uint256(index)].cumulativeAmount;
+            self.items.push(Item({epoch: startVestingEpoch, cumulativeAmount: prevCumulativeAmount}));
+            index++;
+        }
+
+        uint256 thisRewardCumulativeAmount = 0;
+        uint256 lastExistingCumulativeAmount = 0;
         for (uint256 i = 0; i < vestingPeriodCount; i++) {
-            cumulativeAmount += rewardPerPeriod;
+            thisRewardCumulativeAmount += rewardPerPeriod;
             if (index >= 0 && index < int256(length)) {
                 Item storage item = self.items[uint256(index)];
-                item.cumulativeAmount += item.cumulativeAmount;
-                item.cumulativeAmount = cumulativeAmount;
+                // Store last pre-exising cumulative amount (BEFORE adding this reward)
+                lastExistingCumulativeAmount = item.cumulativeAmount;
+                // Add new rewards to the existing item
+                item.cumulativeAmount += thisRewardCumulativeAmount;
                 index++;
             } else {
                 uint256 vestingEpoch = startVestingEpoch + (vestingPeriodDuration * i);
-                self.items.push(Item({epoch: vestingEpoch, cumulativeAmount: cumulativeAmount}));
+                // Keep all pre-exising rewards + add cumulative current reward
+                uint256 newItemCumulativeAmount = lastExistingCumulativeAmount + thisRewardCumulativeAmount;
+                self.items.push(Item({epoch: vestingEpoch, cumulativeAmount: newItemCumulativeAmount}));
             }
         }
     }
 
+    // Returns:
+    // if exists, highest index such that self.items[index].epoch <= withdrawEpoch
+    // -1 otherwise
     function _findClosest(Info storage self, uint256 withdrawEpoch) private view returns (int256 index) {
         uint256 length = self.items.length;
         index = -1;
