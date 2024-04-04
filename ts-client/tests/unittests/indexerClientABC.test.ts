8bf61a899c469cbcd13eb56a794bc04ae195a0c9
alcibiadescleinias <ivan.cheprasov@phystech.edu>
2024-04-04 09:43:42 +0200

    fix test

--- ts-client/tests/unittests/indexerClientABC.test.ts
+++ ts-client/tests/unittests/indexerClientABC.test.ts
@@ -17,7 +17,7 @@ function checkFluenceEnvConfig(stand: string) {
     return
   }
   const allSavedSubgraphUrls = fs.readFileSync(getDeploymentPath(stand), "utf8").split('\n').filter((url) => url.length > 0)
-  const lastSavedSubgraphUrl = allSavedSubgraphUrls[allSavedSubgraphUrls.length - 1];
+  const lastSavedSubgraphUrl = (allSavedSubgraphUrls[allSavedSubgraphUrls.length - 1]).split(' ')[0];
   expect(getIndexerUrl(stand), "Possibly you forgot to update indexerClient/config.ts with the last subgraph URL.").toBe(lastSavedSubgraphUrl)
 }
 
