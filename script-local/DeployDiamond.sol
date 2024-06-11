bea6bb086ef4781b1ed8c7491de9ab067f915628
Dima <crystalbit@mail.ru>
2024-06-11 13:41:34 +0200

    base

---
+++ script-local/DeployDiamond.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.19;
+
+import "forge-std/Script.sol";
+import { Diamond } from "../src/Diamond.sol";
+
+
+contract DeployDiamond is Script {
+  function run () external {
+    vm.startBroadcast();
+    
+  }
+}
