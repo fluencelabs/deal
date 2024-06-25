d2bc7b6c5a322cd50cacccc96e157978f01164e3
folex <0xdxdy@gmail.com>
2024-06-25 10:44:22 +0300

    chore(license): switch to agpl (#457)
    * chore(license): move to agpl
    
    * chore: prettier on markdown

--- src/core/interfaces/IEpochController.sol
+++ src/core/interfaces/IEpochController.sol
@@ -1,4 +1,22 @@
-// SPDX-License-Identifier: Apache-2.0
+/*
+ * Fluence Compute Marketplace
+ *
+ * Copyright (C) 2024 Fluence DAO
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation version 3 of the
+ * License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 pragma solidity ^0.8.19;
 
 /// @title Epoch controller contract interface
